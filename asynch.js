c410bfe435a826f7f0daf4fc86b4559e7dc22ea0
clarine <antibaclarine.com>
2023-05-12 19:43:31 +0300

    first commit

--- asynch.js
+++ asynch.js
@@ -7,25 +7,28 @@ setTimeout(time,5000)
 
 // You have an array of user IDs and a function getUserData(id) that returns a Promise with user data when given a user ID. Write an asynchronous function that fetches and logs the data for each user ID one by one, in sequence
 function getUserData(id){
- let promise =new Promise((resolve,reject)=>)
+    return new Promise((resolve,reject)=>{
  for (const user of UserData) {
-    if(user===id){
-        resolve(user)
+    if(user.id===id){
+        resolve(user);
         return;
     }
  }
- reject("user not found");
+ reject(new Error("user not found"));
+})
 }
+
 
 
 // You have an asynchronous function performTask() that returns a Promise. The Promise resolves if the task is successful and rejects if there's an error. Write a function that calls performTask() and logs a custom success message if the task is successful, and a custom error message if there's an error.
 async function allTask(){
-await performTask()
-try{
-    console.log("task completed successfully");
-}
-catch{
-    console.error('Task failed');
-}
+return new Promise((resolve,reject)=>{
+    if('task is successful'){
+        resolve(data);
+    }else{
+        reject(error)
+    }
+})
 }
 allTask()
+
